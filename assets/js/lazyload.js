fecfb34da5bbafea2112dd23199144415e13e505
ESMNCE <164864255+leseunivers@users.noreply.github.com>
2025-01-30 23:57:42 +0900

    Update lazyload.js

--- assets/js/lazyload.js
+++ assets/js/lazyload.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".lazyload").forEach(img => {
-    img.onload = () => img.classList.add("loaded"); 
-    img.src = img.src; 
+    img.onload = () => img.classList.add("loaded");
   });
 });
