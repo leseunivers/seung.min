d664313e57d47d6067614224660150d16ef001ec
ESMNCE <164864255+leseunivers@users.noreply.github.com>
2025-01-30 21:02:03 +0900

    Update lazyload.js

--- assets/js/lazyload.js
+++ assets/js/lazyload.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const images = document.querySelectorAll(".lazyload");
+  const images = document.querySelectorAll("img");
 
   images.forEach(img => {
     img.onload = function() {
